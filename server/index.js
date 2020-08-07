8d28e489f27826b7881d500fa816628c963d47b6
cjrawlins <caseyrawlins@gmail.com>
2020-08-07 11:17:39 -0600

    Setup section complete

---
+++ server/index.js
@@ -0,0 +1,29 @@
+// Dependencies
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const ctrl = require('./controller');
+
+// Create instance app from express
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Get Database connection info from .env
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+// Invoke massive database connector
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+    }).then(dbInstance => {
+        app.set('db', dbInstance);
+        console.log('Connected to Database')
+        })
+        .catch( error => console.log(error));
+
+//Set up server to listen on port and log
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}.`);
+    });
